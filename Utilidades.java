f3147f789a22927d018fe1b6572c778e304bf836
Prueba1 <Prueba1@hotmail.com>
2021-10-04 22:03:42 +0200

    Utilidades
    Descripcion de la clase utilidades de sus funciones

--- Utilidades.java
+++ Utilidades.java
@@ -12,6 +12,7 @@ public class Utilidades {
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //devuelve un String con los bytes que tiene actualmente
     public static byte[] stringToBytes ( String texto ) {
         return texto.getBytes();
         // byte[] b = string.getBytes(StandardCharsets.UTF_8); // Ja
@@ -19,6 +20,9 @@ public class Utilidades {
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //Funcion para saber el nombre del dispositivo.
+    //tiene un if que sirve por si la longitud que tiene es diferente a 16 no se lo deja poner y salta un error
+    //devuelve el resultado cuando el los bytes son los indicados
     public static UUID stringToUUID( String uuid ) {
         if ( uuid.length() != 16 ) {
             throw new Error( "stringUUID: string no tiene 16 caracteres ");
@@ -50,6 +54,8 @@ public class Utilidades {
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //Entra un Array de bytes que tiene un if que si no encuentra ningun byte devuelve vacio(null)
+    //si encuentra bytes lo que hace es llamar a una funcion que hereda sb y mete cada byte dentro.
     public static String bytesToString( byte[] bytes ) {
         if (bytes == null ) {
             return "";
@@ -64,6 +70,7 @@ public class Utilidades {
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //le entra dos array de reales y los pone juntos y devuelve el array de bytes.
     public static byte[] dosLongToBytes( long masSignificativos, long menosSignificativos ) {
         ByteBuffer buffer = ByteBuffer.allocate( 2 * Long.BYTES );
         buffer.putLong( masSignificativos );
@@ -73,18 +80,22 @@ public class Utilidades {
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //le entra un array de bytes y los devuelve en enteros
     public static int bytesToInt( byte[] bytes ) {
         return new BigInteger(bytes).intValue();
     }
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //le entra un array de bytes y los devuelve en reales
     public static long bytesToLong( byte[] bytes ) {
         return new BigInteger(bytes).longValue();
     }
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //la funcion es si no encuentra bytes devuelve 0
+    //si encuentra mas de 4 bytes le muestra al usuario que demasiados bytes para pasarlos a enteros
     public static int bytesToIntOK( byte[] bytes ) {
         if (bytes == null ) {
             return 0;
@@ -96,7 +107,10 @@ public class Utilidades {
         int res = 0;
 
 
-
+        //recorre todo el array de bytes
+        //res que esta inicializado a 0 es menor de 8
+        //hay un acondicion de que si el byte en la posición 1 del array es diferente a cero le resta 1
+        //devuelve el entero.
         for( byte b : bytes ) {
            /*
            Log.d( MainActivity.ETIQUETA_LOG, "bytesToInt(): byte: hex=" + Integer.toHexString( b )
@@ -123,6 +137,8 @@ public class Utilidades {
 
     // -------------------------------------------------------------------------------
     // -------------------------------------------------------------------------------
+    //si no encuentra byte devuelve null
+    //hereda de una funcion y recorre el array de bytes y los junta y devuelve en formato String
     public static String bytesToHexString( byte[] bytes ) {
 
         if (bytes == null ) {
@@ -141,5 +157,3 @@ public class Utilidades {
 // -----------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------
-
-
