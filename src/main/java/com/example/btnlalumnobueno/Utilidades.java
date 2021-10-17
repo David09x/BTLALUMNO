07c865f1ebd506905887477d884db8498ebd6a90
David09x <71710020+David09x@users.noreply.github.com>
2021-10-17 18:40:02 +0200

    app
    aplicacion

---
+++ src/main/java/com/example/btnlalumnobueno/Utilidades.java
@@ -0,0 +1,158 @@
+package com.example.btnlalumnobueno;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
+// -----------------------------------------------------------------------------------
+// @author: Jordi Bataller i Mascarell
+// -----------------------------------------------------------------------------------
+public class Utilidades {
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //devuelve un String con los bytes que tiene actualmente
+    public static byte[] stringToBytes ( String texto ) {
+        return texto.getBytes();
+        // byte[] b = string.getBytes(StandardCharsets.UTF_8); // Ja
+    } // ()
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //Funcion para saber el nombre del dispositivo.
+    //tiene un if que sirve por si la longitud que tiene es diferente a 16 no se lo deja poner y salta un error
+    //devuelve el resultado cuando el los bytes son los indicados
+    public static UUID stringToUUID( String uuid ) {
+        if ( uuid.length() != 16 ) {
+            throw new Error( "stringUUID: string no tiene 16 caracteres ");
+        }
+        byte[] comoBytes = uuid.getBytes();
+
+        String masSignificativo = uuid.substring(0, 8);
+        String menosSignificativo = uuid.substring(8, 16);
+        UUID res = new UUID( Utilidades.bytesToLong( masSignificativo.getBytes() ), Utilidades.bytesToLong( menosSignificativo.getBytes() ) );
+
+        // Log.d( MainActivity.ETIQUETA_LOG, " \n\n***** stringToUUID *** " + uuid  + "=?=" + Utilidades.uuidToString( res ) );
+
+        // UUID res = UUID.nameUUIDFromBytes( comoBytes ); no va como quiero
+
+        return res;
+    } // ()
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    public static String uuidToString ( UUID uuid ) {
+        return bytesToString( dosLongToBytes( uuid.getMostSignificantBits(), uuid.getLeastSignificantBits() ) );
+    } // ()
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    public static String uuidToHexString ( UUID uuid ) {
+        return bytesToHexString( dosLongToBytes( uuid.getMostSignificantBits(), uuid.getLeastSignificantBits() ) );
+    } // ()
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //Entra un Array de bytes que tiene un if que si no encuentra ningun byte devuelve vacio(null)
+    //si encuentra bytes lo que hace es llamar a una funcion que hereda sb y mete cada byte dentro.
+    public static String bytesToString( byte[] bytes ) {
+        if (bytes == null ) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append( (char) b );
+        }
+        return sb.toString();
+    }
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //le entra dos array de reales y los pone juntos y devuelve el array de bytes.
+    public static byte[] dosLongToBytes( long masSignificativos, long menosSignificativos ) {
+        ByteBuffer buffer = ByteBuffer.allocate( 2 * Long.BYTES );
+        buffer.putLong( masSignificativos );
+        buffer.putLong( menosSignificativos );
+        return buffer.array();
+    }
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //le entra un array de bytes y los devuelve en enteros
+    public static int bytesToInt( byte[] bytes ) {
+        return new BigInteger(bytes).intValue();
+    }
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //le entra un array de bytes y los devuelve en reales
+    public static long bytesToLong( byte[] bytes ) {
+        return new BigInteger(bytes).longValue();
+    }
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //la funcion es si no encuentra bytes devuelve 0
+    //si encuentra mas de 4 bytes le muestra al usuario que demasiados bytes para pasarlos a enteros
+    public static int bytesToIntOK( byte[] bytes ) {
+        if (bytes == null ) {
+            return 0;
+        }
+
+        if ( bytes.length > 4 ) {
+            throw new Error( "demasiados bytes para pasar a int ");
+        }
+        int res = 0;
+
+
+        //recorre todo el array de bytes
+        //res que esta inicializado a 0 es menor de 8
+        //hay un acondicion de que si el byte en la posición 1 del array es diferente a cero le resta 1
+        //devuelve el entero.
+        for( byte b : bytes ) {
+           /*
+           Log.d( MainActivity.ETIQUETA_LOG, "bytesToInt(): byte: hex=" + Integer.toHexString( b )
+                   + " dec=" + b + " bin=" + Integer.toBinaryString( b ) +
+                   " hex=" + Byte.toString( b )
+           );
+           */
+            res =  (res << 8) // * 16
+                    + (b & 0xFF); // para quedarse con 1 byte (2 cuartetos) de lo que haya en b
+        } // for
+
+        if ( (bytes[ 0 ] & 0x8) != 0 ) {
+            // si tiene signo negativo (un 1 a la izquierda del primer byte
+            res = -(~(byte)res)-1; // complemento a 2 (~) de res pero como byte, -1
+        }
+       /*
+        Log.d( MainActivity.ETIQUETA_LOG, "bytesToInt(): res = " + res + " ~res=" + (res ^ 0xffff)
+                + "~res=" + ~((byte) res)
+        );
+        */
+
+        return res;
+    } // ()
+
+    // -------------------------------------------------------------------------------
+    // -------------------------------------------------------------------------------
+    //si no encuentra byte devuelve null
+    //hereda de una funcion y recorre el array de bytes y los junta y devuelve en formato String
+    public static String bytesToHexString( byte[] bytes ) {
+
+        if (bytes == null ) {
+            return "";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+            sb.append(':');
+        }
+        return sb.toString();
+    } // ()
+} // class
+// -----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------
